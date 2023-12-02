398f4af7f20b1dbbb21322a17cbd27e44e550cee
Evilmoose <ronnielopez@gmail.com>
2023-12-01 18:58:13 -0800

    Step 2: Add Company Routes

--- db.js
+++ db.js
@@ -1,2 +1,12 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: "postgresql://ronaldlopez:123456@localhost:5432/biztime"
+});
+
+client.connect();
+
+
+module.exports = client;
